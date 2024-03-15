f46c2bd53b988990099f005643275dc4a11b9052
Max <max015012@gmail.com>
2024-03-15 10:43:23 +0200

    update Main

--- src/com/max015012/Main.java
+++ src/com/max015012/Main.java
@@ -1,6 +1,12 @@
 package com.max015012;
 
+import java.util.Arrays;
+
 public class Main {
+    private static final Runnable[] RUNNABLE_APPS = {
+    };
+
     public static void main(String[] args) {
+        Arrays.stream(RUNNABLE_APPS).forEach(Runnable::run);
     }
 }
